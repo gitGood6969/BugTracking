3413a41a4e2ad4afb0188c6eaa79ebc480c05dca
gitGood6969 <72687984+gitGood6969@users.noreply.github.com>
2020-10-12 15:00:50 +0800

    Create BugTracking.java
    Added the Base code

---
+++ BugTracking.java
@@ -0,0 +1,89 @@
+// Package
+package bugtracking;
+
+// Import Libraries
+import javafx.application.Application; 
+import javafx.event.ActionEvent; 
+import javafx.event.EventHandler; 
+import javafx.scene.Scene; 
+import javafx.scene.control.Button; 
+import javafx.scene.control.Label; 
+//import javafx.scene.layout.StackPane;
+import javafx.stage.Stage; 
+import static javafx.application.Application.launch;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class BugTracking extends Application {    
+ 
+    Scene scene1;
+    Scene scene2;
+    
+    @Override
+    public void start(Stage primaryStage) 
+    {
+        primaryStage.setTitle("UserLoginUI");
+        
+        // Scene1 implementation code here
+        Label labelUsername = new Label("Username:");
+        TextField usernameTextField = new TextField ();
+        Label labelPassword = new Label("Password:");
+        TextField passwordTextField = new TextField ();
+        Button btn1 = new Button();
+        btn1.setText("Login");
+        btn1.setOnAction(new EventHandler<ActionEvent>() 
+        {
+            // Code here handles what happens when the button is clicled.
+            @Override
+            public void handle(ActionEvent event) 
+            {
+                primaryStage.setScene(scene2);
+                System.out.println("User has logged in!");
+            }
+        });
+        // Grouping elements together
+        HBox hbUsername = new HBox(); // For Username Field
+        hbUsername.getChildren().addAll(labelUsername, usernameTextField);
+        hbUsername.setSpacing(10);
+        HBox hbPassword = new HBox(); // For Password Field
+        hbPassword.getChildren().addAll(labelPassword, passwordTextField);
+        hbPassword.setSpacing(10);
+        
+        VBox layout1 = new VBox(20);     
+        layout1.getChildren().addAll(hbUsername, hbPassword, btn1);
+        scene1= new Scene(layout1, 300, 250);
+        
+        // Scene2 implementation
+        Label label2 = new Label("This is the second scene");
+        Button btn2 = new Button();
+        btn2.setText("Logout");
+        btn2.setOnAction(new EventHandler<ActionEvent>() 
+        {
+            // Code here handles what happens when the button is clicled.
+            @Override
+            public void handle(ActionEvent event) 
+            {
+                primaryStage.setScene(scene1);
+                System.out.println("User has logged out!");
+            }
+        });
+        VBox layout2 = new VBox(20);     
+        layout2.getChildren().addAll(label2, btn2);
+        scene2= new Scene(layout2, 300, 250);
+
+        primaryStage.setScene(scene1);
+        primaryStage.show();
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+}  
+
+/* Websites used:
+http://www.learningaboutelectronics.com/Articles/How-to-create-multiple-scenes-and-switch-between-scenes-in-JavaFX.php
+https://docs.oracle.com/javafx/2/ui_controls/text-field.htm
+
+
+*/
